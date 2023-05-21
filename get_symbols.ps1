76a6c3713ec8a91617038160a5963388d34b8db4
GilbertNordhammar <gilbertnordhammar@gmail.com>
2023-05-21 23:16:53 +0200

    added example 2

--- get_symbols.ps1
+++ get_symbols.ps1
@@ -3,13 +3,12 @@ $keyword = $args[1]
 
 $keywords = $args[1..($args.Length - 1)]
 $searchTerm = ($keywords | ForEach-Object { "/C:$_" }) -join " "
-Write-Output $searchTerm
 
 Get-ChildItem -Path .\$target -Filter *.cpp -Recurse -File -Name | ForEach-Object {
     $cpp_file = $_.replace("src\", "")
     $obj_file = $cpp_file.replace(".cpp", ".obj")
 
-    Write-Output "======= $cpp_file ======="
+    Write-Output "======= $obj_file ======="
     $command = "dumpbin /symbols build\$target\$target.dir\Debug\$obj_file | findstr /i $searchTerm | findstr /i /v unwind"
     Invoke-Expression $command
     Write-Output ""
