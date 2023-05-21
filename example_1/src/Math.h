f9716e063f72fb37555eb1c74188efb49bf637f0
GilbertNordhammar <gilbertnordhammar@gmail.com>
2023-05-21 23:35:42 +0200

    added auto argument fucntion to example 1

--- example_1/src/Math.h
+++ example_1/src/Math.h
@@ -14,6 +14,12 @@ auto Mul(T1 a, T2 b)
     return a * b;
 }
 
+// auto arguments are supported as of C++ 20
+// auto Div(auto a, auto b)
+// {
+//     return a / b;
+// }
+
 template <typename T>
 T Sub(T a, T b)
 {
